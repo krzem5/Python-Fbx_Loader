6f14eb06e814d8ae466352aaca214d59a575e1fe
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-09-17 20:27:58 +0200

    Push Update 09/17/2020, 20:27:51

--- fbx2timeline.py
+++ fbx2timeline.py
@@ -2,6 +2,7 @@ import os
 import struct
 import json
 import zlib
+import math
 
 
 
@@ -111,13 +112,13 @@ def _get_prop70(o,nm):
 
 
 def _get_frame(fps,f):
-	return round(f//46186158/(1000/fps))
+	return math.ceil(((f-fps[1])//46186158)/(1000/fps[0]))
 
 
 
 def _get_ref(cl,ol,id_,k=None):
 	if (k==-1):
-		return [ol[e[0]] for e in cl[id_]]
+		return [(e[1],ol[e[0]]) for e in cl[id_]]
 	for e in cl[id_]:
 		if (e[1]==k):
 			return ol[e[0]]
@@ -125,21 +126,74 @@ def _get_ref(cl,ol,id_,k=None):
 
 
 
-def _get_model(cl,ol,id_):
-	m=_get_ref(cl,ol,id_)
-	o={"name":m["name"]}
-	print(o,_get_child(m,"Properties70"))
+def _get_model(fps,cl,ol,m):
+	p=_get_prop70(_get_child(m,"Properties70"),"Lcl Translation")
+	r=_get_prop70(_get_child(m,"Properties70"),"Lcl Rotation")
+	o={"name":m["name"],"data":{"x":p[0],"y":p[1],"z":p[2],"rx":r[0],"ry":r[1],"rz":r[2]},"children":[]}
+	for et,e in _get_ref(cl,ol,m["id"],-1):
+		if (e["type"]=="Model"):
+			o["children"]+=[_get_model(fps,cl,ol,e)]
+		elif (e["type"]=="AnimationCurveNode"):
+			al=_get_ref(cl,ol,e["id"],-1)
+			for t,k in al:
+				if (len(t)!=3 or t[:2]!="d|" or t[2] not in "xyzXYZ"):
+					raise RuntimeError
+				kl=_get_child(k,"KeyTime")["data"][0]
+				kv=_get_child(k,"KeyValueFloat")["data"][0]
+				o["data"][("" if et=="Lcl Translation" else "r")+t[2].lower()]=([] if kl[0]==0 else [(0,o["data"][("" if et=="Lcl Translation" else "r")+t[2].lower()])])
+				l=None
+				for i,v in enumerate(kl):
+					if (l!=None and l<_get_frame(fps,v)-1):
+						print(m["name"],i,kl[i-1]//46186158,kl[i]//46186158,_get_frame(fps,kl[i-1]),_get_frame(fps,v)-1)
+						raise RuntimeError
+					o["data"][("" if et=="Lcl Translation" else "r")+t[2].lower()]+=[kv[i]]
+					l=_get_frame(fps,v)
 	return o
 
 
 
-for k in os.listdir("."):
-	if (k[-4:]==".fbx"):
-		with open(k,"rb") as f:
+def _write_model(f,fps,cl,ol,m):
+	p=_get_prop70(_get_child(m,"Properties70"),"Lcl Translation")
+	r=_get_prop70(_get_child(m,"Properties70"),"Lcl Rotation")
+	l=_get_ref(cl,ol,m["id"],-1)[:255]
+	dt={"x":p[0],"y":p[1],"z":p[2],"rx":r[0],"ry":r[1],"rz":r[2]}
+	fl=0
+	c=0
+	for et,e in l:
+		if (e["type"]=="Model"):
+			c+=1
+		elif (e["type"]=="AnimationCurveNode"):
+			al=_get_ref(cl,ol,e["id"],-1)
+			for t,k in al:
+				if (len(t)!=3 or t[:2]!="d|" or t[2] not in "xyzXYZ"):
+					raise RuntimeError
+				kl=_get_child(k,"KeyTime")["data"][0]
+				kv=_get_child(k,"KeyValueFloat")["data"][0]
+				dt[("" if et=="Lcl Translation" else "r")+t[2].lower()]=([] if kl[0]==0 else [(0,dt[("" if et=="Lcl Translation" else "r")+t[2].lower()])])
+				lk=None
+				for i,v in enumerate(kl):
+					if (lk!=None and lk<_get_frame(fps,v)-1):
+						print(m["name"],i,kl[i-1]//46186158,kl[i]//46186158,_get_frame(fps,kl[i-1]),_get_frame(fps,v)-1)
+						raise RuntimeError
+					dt[("" if et=="Lcl Translation" else "r")+t[2].lower()]+=[kv[i]]
+					lk=_get_frame(fps,v)
+				if (len(dt[("" if et=="Lcl Translation" else "r")+t[2].lower()])>1):
+					fl&=(1<<(ord(t[2].lower())-120+(0 if et=="Lcl Translation" else 3)))
+	f.write(len(m["name"][:255]).to_bytes(1,"big")+bytes(m["name"],"utf-8")+fl.to_bytes(1,"big")+c.to_bytes(1,"big"))
+	for et,e in l:
+		if (e["type"]=="Model"):
+			pass
+			# ch+=[_get_model(fps,cl,ol,e)]
+
+
+
+for fp in os.listdir("."):
+	if (fp[-4:]==".fbx"):
+		with open(fp,"rb") as f:
 			dt=f.read()
 		if (dt[:len(HEAD_MAGIC)]!=HEAD_MAGIC):
 			continue
-		with open(f"{k[:-4]}-timeline.json","w") as f:
+		with open(f"{fp[:-4]}.anm","wb") as f:
 			i=len(HEAD_MAGIC)+4
 			gs=None
 			ol=None
@@ -162,24 +216,30 @@ for k in os.listdir("."):
 					df={}
 					for k in e["children"]:
 						if (k["name"]=="ObjectType" and k["data"][0]!="GlobalSettings"):
-							df[k["data"][0]]=_get_child(k,"PropertyTemplate")
-							raise RuntimeError
+							if (_get_child(k,"PropertyTemplate")!=None):
+								df[k["data"][0]]=_get_child(_get_child(k,"PropertyTemplate"),"Properties70")["children"]
+							else:
+								df[k["data"][0]]=[]
 				elif (e["name"]=="Connections"):
 					cl={}
 					for c in e["children"]:
 						if (c["data"][2] not in list(cl.keys())):
 							cl[c["data"][2]]=[]
 						cl[c["data"][2]]+=[[c["data"][1],(None if len(c["data"])==3 else c["data"][3])]]
-			fps=["default",120,100,60,50,48,30,30,"drop",29.97,25,24,"1000 milli/s",23.976,"custom",96,72,59.94,"time-modes"][_get_prop70(_get_child(gs,"Properties70"),"TimeMode")[0]]
-			o={"name":as_["name"],"fps":fps,"start_frame":_get_frame(fps,_get_prop70(_get_child(gs,"Properties70"),"TimeSpanStart")[0]),"end_frame":_get_frame(fps,_get_prop70(_get_child(gs,"Properties70"),"TimeSpanStop")[0]),"model":_get_model(cl,ol,0)}
-			# for l in _get_ref(cl,ol,as_["id"],-1):
-			# 	for n in _get_ref(cl,ol,l["id"],-1):
-			# 		o["nodes"]+=[{"name":n["name"],"channels":{}}]
-			# 		for p in _get_child(n,"Properties70")["children"]:
-			# 			o["nodes"][-1]["channels"][p["data"][0]]=[]
-			# 			c=_get_ref(cl,ol,n["id"],p["data"][0])
-			# 			kl=_get_child(c,"KeyTime")["data"][0]
-			# 			kv=_get_child(c,"KeyValueFloat")["data"][0]
-			# 			for i,t in enumerate(kl):
-			# 				o["nodes"][-1]["channels"][p["data"][0]]+=[(_get_frame(fps,t),kv[i])]
-			f.write(json.dumps(o,indent=4,sort_keys=False).replace("    ","\t"))
+			for k,v in ol.items():
+				ch=_get_child(v,"Properties70")
+				kn=[]
+				if (ch==None):
+					v["children"]+=[{"name":"Properties70","children":[]}]
+					ch=v["children"][-1]
+				else:
+					for e in ch["children"]:
+						kn+=[e["data"][0]]
+				for e in df[v["type"]]:
+					if (e["data"][0] not in kn):
+						kn+=[e["data"][0]]
+						ch["children"]+=[e]
+				ol[k]=v
+			fps=(["default",120,100,60,50,48,30,30,"drop",29.97,25,24,"1000 milli/s",23.976,"custom",96,72,59.94,"time-modes"][_get_prop70(_get_child(gs,"Properties70"),"TimeMode")[0]],_get_prop70(_get_child(gs,"Properties70"),"TimeSpanStart")[0])
+			f.write(_get_prop70(_get_child(gs,"Properties70"),"TimeMode")[0].to_bytes(1,"big")+len(as_["name"][:255]).to_bytes(1,"big")+bytes(as_["name"],"utf-8")+_get_frame(fps,_get_prop70(_get_child(gs,"Properties70"),"TimeSpanStop")[0]).to_bytes(2,"big"))
+			_write_model(f,fps,cl,ol,_get_ref(cl,ol,0))
