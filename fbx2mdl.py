bd4f38cbd65bb06f9fc266f8ad6a1a13dc74316e
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-09-26 16:20:23 +0200

    Push Update 09/26/2020, 16:20:18

--- fbx2mdl.py
+++ fbx2mdl.py
@@ -260,7 +260,7 @@ def _write_poses(f,cl,ol,pl):
 				if (m!=None):
 					raise RuntimeError("Duplicate Material!")
 				print(" "*bi+"Merging Data...")
-				m={"c":{"a":_get_prop70(_get_child(e,"Properties70"),"AmbientColor"),"d":_get_prop70(_get_child(e,"Properties70"),"DiffuseColor"),"s":_get_prop70(_get_child(e,"Properties70"),"SpecularColor")},"df":_get_prop70(_get_child(e,"Properties70"),"DiffuseFactor")[0],"se":_get_prop70(_get_child(e,"Properties70"),"ShininessExponent")[0],"d":_get_prop70(_get_child(e,"Properties70"),"Diffuse"),"s":_get_prop70(_get_child(e,"Properties70"),"Specular")}
+				m={"c":{"a":_get_prop70(_get_child(e,"Properties70"),"AmbientColor"),"d":_get_prop70(_get_child(e,"Properties70"),"DiffuseColor"),"s":_get_prop70(_get_child(e,"Properties70"),"SpecularColor")},"df":_get_prop70(_get_child(e,"Properties70"),"DiffuseFactor")[0],"se":_get_prop70(_get_child(e,"Properties70"),"ShininessExponent")[0]}
 			elif (e["type"]=="AnimationCurveNode"):
 				print(" "*bi+"Parsing Default Offsets...")
 				if (et=="Lcl Translation"):
@@ -310,7 +310,6 @@ def _write_poses(f,cl,ol,pl):
 			k["deform"]["indexes"]+=mp[e]
 			k["deform"]["weights"]+=[tw[j]]*len(mp[e])
 		nm=_name(k["name"])
-		print(nm,len(k['deform']['indexes']))
 		f.write(struct.pack(f"<B{len(nm[:255])}sfB6fI{len(k['deform']['indexes'])}H{len(k['deform']['indexes'])}f",len(nm[:255]),bytes(nm[:255],"utf-8"),k["len"],len(k["children"]),k["dx"],k["dy"],k["dz"],k["drx"],k["dry"],k["drz"],len(k["deform"]["indexes"]),*k["deform"]["indexes"],*k["deform"]["weights"]))
 		for e in k["children"]:
 			_write_mdl(f,e,i+2,mp)
@@ -381,7 +380,7 @@ def _write_poses(f,cl,ol,pl):
 			il+=[vhl.index(hash(v))]
 		print(f"      100% Complete ({len(dtl)//STRIDE}v, {len(il)//3}i)...\n    Writing To File...")
 		nm=_name(p["name"])
-		f.write(struct.pack(f"<B{len(nm)}sBII{len(dtl)+17}f{len(il)}H",len(nm),bytes(nm,"utf-8"),len(list(l.keys())),len(dtl)//STRIDE,len(il),*m["c"]["a"],*m["c"]["d"],*m["c"]["s"],m["df"],m["se"],*m["d"],*m["s"],*dtl,*il))
+		f.write(struct.pack(f"<B{len(nm)}sBII{len(dtl)+11}f{len(il)}H",len(nm),bytes(nm,"utf-8"),len(list(l.keys())),len(dtl)//STRIDE,len(il),*m["c"]["a"],*m["c"]["d"],*m["c"]["s"],m["df"],m["se"],*dtl,*il))
 		print("    Writing Models To File...")
 		for k in l.values():
 			_write_mdl(f,k,6,mp)
